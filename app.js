a501696a0f7c98c763e51e7a4aa7e5283370c7fb
ayoafolalu <ayo.afolalu.yahoo.co.uk>
2020-07-27 22:15:10 +0000

    add watch product pages

--- app.js
+++ app.js
@@ -42,6 +42,68 @@ app.get("/timeless",function(req,res)
 		res.render("timeless");
 		});
 
+app.get("/explorer",function(req,res)
+{
+		res.render("explorer");
+		});
+
+app.get("/perpetual",function(req,res)
+{
+		res.render("perpetual");
+		});
+
+app.get("/stardustgold",function(req,res)
+{
+		res.render("stardustgold");
+		});
+
+app.get("/cosmographdaytona",function(req,res)
+{
+		res.render("cosmographdaytona");
+		});
+
+app.get("/greyscale",function(req,res)
+{
+		res.render("greyscale");
+		});
+
+app.get("/diamondgoldlink",function(req,res)
+{
+		res.render("diamondgoldlink");
+		});
+
+
+app.get("/wellington",function(req,res)
+{
+		res.render("wellington");
+		});
+
+app.get("/braun",function(req,res)
+{
+		res.render("braun");
+		});
+
+app.get("/nixonrosegold",function(req,res)
+{
+		res.render("nixonrosegold");
+		});
+
+app.get("/raymondwell",function(req,res)
+{
+		res.render("raymondwell");
+		});
+
+app.get("/goldsparkle",function(req,res)
+{
+		res.render("goldsparkle");
+		});
+
+app.get("/oldtowngold",function(req,res)
+{
+		res.render("oldtowngold");
+		});
+
+
 app.get("/*",function(req,res)
 {
 		res.render("home");
